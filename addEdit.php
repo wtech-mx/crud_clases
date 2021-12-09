011e357ebc2629f1284f902c96d97c5735af243f
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2021-12-09 14:28:57 -0600

    Actualizacion de vistas

--- addEdit.php
+++ addEdit.php
@@ -27,73 +27,152 @@ if (!empty($sessData['status']['msg'])) {
 }
 ?>
 
+<style>
+    .form-intern {
+    width: 98%;
+    height: auto;
+    padding: 20px 15px;
+    display: block;
+    margin: 10px;
+    background-color: #d0e5e9;
+    border: 1px solid #075D9E;
+    display: inline-block;
+    overflow: auto;
+}
+sectionCampos.Col2 {
+    padding-top: 0px;
+    min-height: 50px;
+    padding-bottom: 5px;
+}
+.sectionCampos {
+    width: 48%;
+    float: left;
+    min-height: 80px;
+    padding: 1%;
+}
+</style>
+
+<section id="cuerpo-container-app">         
+        <section class="content-header navbar-light">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    
+                <div class="col-sm-6"> 
+                    <h1> Transporte </h1>
+                </div>
+
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="../../">Inicio</a></li>
+                    <li class="breadcrumb-item active">Transporte</li>
+                    </ol>
+                </div>
+
+            </div>
+            </div>
+</section> 
+
 <div class="container">
     <?php if (!empty($statusMsg) && ($statusMsgType == 'success')) { ?>
         <div class="alert alert-success"><?php echo $statusMsg; ?></div>
     <?php } elseif (!empty($statusMsg) && ($statusMsgType == 'error')) { ?>
         <div class="alert alert-danger"><?php echo $statusMsg; ?></div>
     <?php } ?>
-    <div class="row">
-        <div class="panel panel-default users-content">
-            <div class="panel-heading"><?php echo $actionLabel; ?> User <a href="index.php" class="glyphicon glyphicon-arrow-left"></a></div>
-            <div class="panel-body">
+
+<div class="row">
+
+            <section class="form-intern" style="max-width:1120px; margin-top:0px; display:inline-block;">
+
                 <form method="post" action="userAction.php" class="form">
-                    <div class="form-group">
-                        <label>Placa</label>
-                        <input type="text" class="form-control" name="placa" value="<?php echo !empty($userData['placa']) ? $userData['placa'] : ''; ?>">
-                    </div>
-                    <div class="form-group">
-                        <label>Año</label>
-                        <input type="text" class="form-control" name="anio" value="<?php echo !empty($userData['anio']) ? $userData['anio'] : ''; ?>">
-                    </div>
-                    <div class="form-group">
-                        <label>Tipo</label>
-                        <input type="text" class="form-control" name="tipo" value="<?php echo !empty($userData['tipo']) ? $userData['tipo'] : ''; ?>">
-                    </div>
 
-                    <div class="form-group">
-                        <label>Permiso SCT</label>
-                        <input type="text" class="form-control" name="permisosct" value="<?php echo !empty($userData['permisosct']) ? $userData['permisosct'] : ''; ?>">
-                    </div>
-                    <div class="form-group">
-                        <label>Numero Permiso SCT</label>
-                        <input type="text" class="form-control" name="numeropermisosct" value="<?php echo !empty($userData['numeropermisosct']) ? $userData['numeropermisosct'] : ''; ?>">
-                    </div>
-                    <div class="form-group">
-                        <label>Nombrea Seguradora Responsabilidad Civil</label>
-                        <input type="text" class="form-control" name="nombreaseguradoraresponsabilidadcivil" value="<?php echo !empty($userData['nombreaseguradoraresponsabilidadcivil']) ? $userData['nombreaseguradoraresponsabilidadcivil'] : ''; ?>">
-                    </div>
 
-                    <div class="form-group">
-                        <label>Numero Poliza Responsabilidad Civil</label>
-                        <input type="text" class="form-control" name="numeropolizaresponsabilidadcivil" value="<?php echo !empty($userData['numeropolizaresponsabilidadcivil']) ? $userData['numeropolizaresponsabilidadcivil'] : ''; ?>">
-                    </div>
-                    <div class="form-group">
-                        <label>Nombrea Seguradora Carga</label>
-                        <input type="text" class="form-control" name="nombreaseguradoracarga" value="<?php echo !empty($userData['nombreaseguradoracarga']) ? $userData['nombreaseguradoracarga'] : ''; ?>">
-                    </div>
-                    <div class="form-group">
-                        <label>Numero Poliza Carga</label>
-                        <input type="text" class="form-control" name="numeropolizacarga" value="<?php echo !empty($userData['numeropolizacarga']) ? $userData['numeropolizacarga'] : ''; ?>">
-                    </div>
+                    <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Placa</label>
+                            <div class="col-sm-2">
+                                <input type="text" class="form-control" name="placa" value="<?php echo !empty($userData['placa']) ? $userData['placa'] : ''; ?>">
+                            </div>
 
-                    <div class="form-group">
-                        <label>Nombre Aseguradora Medio Ambiente</label>
-                        <input type="text" class="form-control" name="nombreaseguradoramedioambiente" value="<?php echo !empty($userData['nombreaseguradoramedioambiente']) ? $userData['nombreaseguradoramedioambiente'] : ''; ?>">
+                            <label class="col-sm-2 col-form-label">Año</label>
+                            <div class="col-sm-2">
+                                <input type="text" class="form-control" name="anio" value="<?php echo !empty($userData['anio']) ? $userData['anio'] : ''; ?>">
+                            </div>
+
+                            <label class="col-sm-2 col-form-label">Tipo</label>
+                            <div class="col-sm-2">
+                                <input type="text" class="form-control" name="tipo" value="<?php echo !empty($userData['tipo']) ? $userData['tipo'] : ''; ?>">
+                            </div>
                     </div>
-                    <div class="form-group">
-                        <label>Numero Poliza Medio Ambiente</label>
-                        <input type="text" class="form-control" name="numeropolizamedioambiente" value="<?php echo !empty($userData['numeropolizamedioambiente']) ? $userData['numeropolizamedioambiente'] : ''; ?>">
+                                        
+
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label">Permiso SCT</label>
+                            <div class="col-sm-2">
+                                <input type="text" class="form-control" name="permisosct" value="<?php echo !empty($userData['permisosct']) ? $userData['permisosct'] : ''; ?>">
+                            </div>
+
+                            <label class="col-sm-2 col-form-label">Numero Permiso SCT</label>
+                            <div class="col-sm-2">
+                                <input type="text" class="form-control" name="numeropermisosct" value="<?php echo !empty($userData['numeropermisosct']) ? $userData['numeropermisosct'] : ''; ?>">
+                            </div>
+
+                            <label class="col-sm-2 col-form-label">Nombrea Seguradora Responsabilidad Civil</label>
+                            <div class="col-sm-2">
+                                <input type="text" class="form-control" name="nombreaseguradoraresponsabilidadcivil" value="<?php echo !empty($userData['nombreaseguradoraresponsabilidadcivil']) ? $userData['nombreaseguradoraresponsabilidadcivil'] : ''; ?>">
+                            </div>
                     </div>
-                    <div class="form-group">
-                        <label>Prima Seguro</label>
-                        <input type="text" class="form-control" name="primaseguro" value="<?php echo !empty($userData['primaseguro']) ? $userData['primaseguro'] : ''; ?>">
+                                        
+
+                    <div class="form-group  row">
+                        <label class="col-sm-2 col-form-label">Numero Poliza Responsabilidad Civil</label>
+                            <div class="col-sm-2">
+                                <input type="text" class="form-control" name="numeropolizaresponsabilidadcivil" value="<?php echo !empty($userData['numeropolizaresponsabilidadcivil']) ? $userData['numeropolizaresponsabilidadcivil'] : ''; ?>">
+                            </div>
+
+                            <label class="col-sm-2 col-form-label">Nombrea Seguradora Carga</label>
+                            <div class="col-sm-2">
+                                <input type="text" class="form-control" name="nombreaseguradoracarga" value="<?php echo !empty($userData['nombreaseguradoracarga']) ? $userData['nombreaseguradoracarga'] : ''; ?>">
+                            </div>
+
+                            <label class="col-sm-2 col-form-label">Numero Poliza Carga</label>
+                            <div class="col-sm-2">
+                                <input type="text" class="form-control" name="numeropolizacarga" value="<?php echo !empty($userData['numeropolizacarga']) ? $userData['numeropolizacarga'] : ''; ?>">
+                            </div>
+
                     </div>
+                                 
+
+                    <div class="form-group  row">
+                        <label class="col-sm-2 col-form-label">Numero Poliza Carga</label>
+                            <div class="col-sm-4">
+                                <input type="text" class="form-control" name="numeropolizacarga" value="<?php echo !empty($userData['numeropolizacarga']) ? $userData['numeropolizacarga'] : ''; ?>">
+                            </div>
+
+                            <label class="col-sm-2 col-form-label">Nombre Aseguradora Medio Ambiente</label>
+                            <div class="col-sm-4">
+                                <input type="text" class="form-control" name="nombreaseguradoramedioambiente" value="<?php echo !empty($userData['nombreaseguradoramedioambiente']) ? $userData['nombreaseguradoramedioambiente'] : ''; ?>">
+                            </div>
+
+                    </div>
+                                        
+                    
+                    <div class="form-group  row">
+                    <label class="col-sm-2 col-form-label">Numero Poliza Medio Ambiente</label>
+                            <div class="col-sm-4">
+                                <input type="text" class="form-control" name="numeropolizamedioambiente" value="<?php echo !empty($userData['numeropolizamedioambiente']) ? $userData['numeropolizamedioambiente'] : ''; ?>">
+                            </div>
+                            
+                        <label class="col-sm-2 col-form-label">Prima Seguro</label>
+                            <div class="col-sm-4">
+                                <input type="text" class="form-control" name="primaseguro" value="<?php echo !empty($userData['primaseguro']) ? $userData['primaseguro'] : ''; ?>">
+                            </div>
+                    </div>
+                    
 
                     <input type="hidden" name="id" value="<?php echo !empty($userData['id']) ? $userData['id'] : ''; ?>">
-                    <input type="submit" name="userSubmit" class="btn btn-success" value="SUBMIT" />
+                    <input type="submit" name="userSubmit" class="btn btn-success" value="Guardar" />
+                    
                 </form>
-            </div>
+            </section>
+
+
         </div>
-    </div>
-</div>
--- index.php
+++ index.php
@@ -10,8 +10,8 @@ $sessData = !empty($_SESSION['sessData']) ? $_SESSION['sessData'] : '';
 require_once 'DB.class.php';
 $db = new DB();
 
-//get users from database
-$users = $db->getRows('cporte_tvehiculos', array('order_by' => 'id DESC'));
+//get register from database
+$register = $db->getRows('cporte_tvehiculos', array('order_by' => 'id DESC'));
 
 //get status message from session
 if (!empty($sessData['status']['msg'])) {
@@ -21,68 +21,109 @@ if (!empty($sessData['status']['msg'])) {
 }
 ?>
 
-<div class="container">
-    <?php if (!empty($statusMsg) && ($statusMsgType == 'success')) { ?>
-        <div class="alert alert-success"><?php echo $statusMsg; ?></div>
-    <?php } elseif (!empty($statusMsg) && ($statusMsgType == 'error')) { ?>
-        <div class="alert alert-danger"><?php echo $statusMsg; ?></div>
-    <?php } ?>
-    <div class="row">
-        <div class="panel panel-default users-content">
-            <div class="panel-heading">Users <a href="addEdit.php" class="glyphicon glyphicon-plus"></a></div>
-            <table id="example" class="table table-striped table-bordered" style="width:100%">
-                <thead>
-                    <tr>
-                        <th></th>
-						<th class='text-center'>Placa</th>
-						<th class='text-center'>Año </th>
-						<th class='text-center'>Tipo </th>
-						<th class='text-center'>Permiso SCT</th>
-						<th class='text-center'># Permiso</th>
-						<th class='text-center'>Aseguradora resp. civil</th>
-                        <th class='text-center'># Poliza resp. civil</th>
-						<th class='text-center'>Aseguradora de la carga</th>
-						<th class='text-center'># Póliza de la carga</th>
-						<th class='text-center'>Aseguradora del medio ambiente</th>
-						<th class='text-center'># Póliza medio ambiente</th>
-						<th class='text-center'>Prima del seguro</th>
-						<th></th>
-                    </tr>
-                </thead>
-                <tbody id="userData">
-                    <?php if (!empty($users)) : $count = 0;
-                        foreach ($users as $user) : $count++; ?>
-                            <tr>
-                                <td><?php echo '#' . $count; ?></td>
-                                <td><?php echo $user['placa']; ?></td>
-                                <td><?php echo $user['anio']; ?></td>
-                                <td><?php echo $user['tipo']; ?></td>
-                                <td><?php echo $user['permisosct']; ?></td>
-                                <td><?php echo $user['numeropermisosct']; ?></td>
-                                <td><?php echo $user['nombreaseguradoraresponsabilidadcivil']; ?></td>
-                                <td><?php echo $user['numeropolizaresponsabilidadcivil']; ?></td>
-                                <td><?php echo $user['nombreaseguradoracarga']; ?></td>
-                                <td><?php echo $user['numeropolizacarga']; ?></td>
-                                <td><?php echo $user['nombreaseguradoramedioambiente']; ?></td>
-                                <td><?php echo $user['numeropolizamedioambiente']; ?></td>
-                                <td><?php echo $user['primaseguro']; ?></td>
-                                <td>
-                                    <a href="addEdit.php?id=<?php echo $user['id']; ?>" class="glyphicon glyphicon-edit"></a>
-                                    <a href="userAction.php?action_type=delete&id=<?php echo $user['id']; ?>" class="glyphicon glyphicon-trash" onclick="return confirm('Are you sure to delete?')"></a>
-                                </td>
-                            </tr>
-                        <?php endforeach;
-                    else : ?>
-                        <tr>
-                            <td colspan="5">No user(s) found......</td>
-                        </tr>
-                    <?php endif; ?>
-                </tbody>
-            </table>
+<section id="cuerpo-container-app">         
+        <section class="content-header navbar-light">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    
+                <div class="col-sm-6"> 
+                    <h1> Dashboard </h1>
+                </div>
+
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="../../">Inicio</a></li>
+                    <li class="breadcrumb-item active">Dashboard</li>
+                    </ol>
+                </div>
+
+            </div>
+            </div>
+</section>
+
+<div style="display:inline-block; width:100%; overflow:auto; text-align:center; padding: 0px 30px 50px 30px; ">
+    <div class="cont-regCfdi" style="width: auto; display:inline-block;">  
+        <div class="content" style="width: auto; display:inline-block;">
+            <div class="griDregCFD" style="overflow-y:hidden; ">
+
+            <!-- tabla -->
+            <div class="container">
+                <?php if (!empty($statusMsg) && ($statusMsgType == 'success')) { ?>
+                    <div class="alert alert-success"><?php echo $statusMsg; ?></div>
+                <?php } elseif (!empty($statusMsg) && ($statusMsgType == 'error')) { ?>
+                    <div class="alert alert-danger"><?php echo $statusMsg; ?></div>
+                <?php } ?>
+                <div class="row">
+                    <div class="panel panel-default register-content">
+                        <div class="panel-heading">
+                            Transporte 
+                            <a href="addEdit.php" class="glyphicon glyphicon-plus"></a>
+                    </div>
+                        <table id="example" class="table table-striped table-bordered" style="width:100%">
+                            <thead>
+                                <tr>
+                                    <th></th>
+                                    <th class='text-center'>Placa</th>
+                                    <th class='text-center'>Año </th>
+                                    <th class='text-center'>Tipo </th>
+                                    <th class='text-center'>Permiso SCT</th>
+                                    <th class='text-center'># Permiso</th>
+                                    <th class='text-center'>Aseguradora resp. civil</th>
+                                    <th class='text-center'># Poliza resp. civil</th>
+                                    <th class='text-center'>Aseguradora de la carga</th>
+                                    <th class='text-center'># Póliza de la carga</th>
+                                    <th class='text-center'>Aseguradora del medio ambiente</th>
+                                    <th class='text-center'># Póliza medio ambiente</th>
+                                    <th class='text-center'>Prima del seguro</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody id="userData">
+                                <?php if (!empty($register)) : $count = 0;
+                                    foreach ($register as $transporte) : $count++; ?>
+                                        <tr>
+                                            <td><?php echo '#' . $count; ?></td>
+                                            <td><?php echo $transporte['placa']; ?></td>
+                                            <td><?php echo $transporte['anio']; ?></td>
+                                            <td><?php echo $transporte['tipo']; ?></td>
+                                            <td><?php echo $transporte['permisosct']; ?></td>
+                                            <td><?php echo $transporte['numeropermisosct']; ?></td>
+                                            <td><?php echo $transporte['nombreaseguradoraresponsabilidadcivil']; ?></td>
+                                            <td><?php echo $transporte['numeropolizaresponsabilidadcivil']; ?></td>
+                                            <td><?php echo $transporte['nombreaseguradoracarga']; ?></td>
+                                            <td><?php echo $transporte['numeropolizacarga']; ?></td>
+                                            <td><?php echo $transporte['nombreaseguradoramedioambiente']; ?></td>
+                                            <td><?php echo $transporte['numeropolizamedioambiente']; ?></td>
+                                            <td><?php echo $transporte['primaseguro']; ?></td>
+                                            <td>
+                                                <a href="addEdit.php?id=<?php echo $user['id']; ?>" class="glyphicon glyphicon-edit"></a>
+                                                <a href="userAction.php?action_type=delete&id=<?php echo $user['id']; ?>" class="glyphicon glyphicon-trash" onclick="return confirm('Are you sure to delete?')"></a>
+                                            </td>
+                                        </tr>
+                                    <?php endforeach;
+                                else : ?>
+                                    <tr>
+                                        <td colspan="5">No Registers(s) found......</td>
+                                    </tr>
+                                <?php endif; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <!-- table -->
+
+            </div>
         </div>
+
     </div>
+
 </div>
+
+
+
 <script>
+
 $(document).ready(function() {
     $('#example').DataTable();
 } );
